3e3bc924a377ea85edcb20b7e943f83274b3c928
Ritvik-Singh-2004 <ritviksingh2004@gmail.com>
2026-07-18 15:24:43 +0530

    feat: Added Pydantic based input verification and CORS middleware for communication between frontend and backend

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware # NEW IMPORT
+from llm_engine import generate_linkedin_post
 
-# init an app
-app=FastAPI(title="LinkedIn GhostWriter Engine")
+app = FastAPI(title="LinkedIn Ghostwriter API")
 
-#GET method
+# NEW: The CORS Bouncer
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # In production, we restrict this. For local testing, "*" allows all.
+    allow_credentials=True,
+    allow_methods=["*"], # Allows POST, GET, etc.
+    allow_headers=["*"], # Allows all headers
+)
+
+class PostRequest(BaseModel):
+    topic: str
+
 @app.get("/")
-
 def read_root():
-    return {
-        "status":"Online",
-        "message":"Welcome to the LinkedIn GhostWriter API"
-    }
+    return {"status": "Online"}
+
+@app.post("/generate")
+def create_post(request: PostRequest):
+    return {"engaging_linkedin_post": generate_linkedin_post(request.topic)}
